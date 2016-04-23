b3bc5703ac2f7b2427fe4043bae1b6874620d64e
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-04-23 13:25:37 +0000

    Refactored to enrollment-wall to be more DRY

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,13 +8,18 @@ class LessonsController < ApplicationController
   private
   
   def require_authorized_for_current_lesson
-    if !current_user.enrolled_in?(current_lesson.section.course)
-      redirect_to course_path(current_lesson.section.course), alert: 'Enroll to view lessons for this course'
+    if !current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'Enroll to view lessons for this course'
     end
   end
   
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+  
+  helper_method :current_course
+  def current_course
+    @current_course ||= current_lesson.section.course
   end
 end
